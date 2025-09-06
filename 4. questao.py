2ed65083a29587ce8a3e257a9f1a4bf724712824
kaikrenx <fjoaopedro03@gmail.com>
2025-09-06 11:01:39 -0300

    Update 4. questao.py

--- 4. questao.py
+++ 4. questao.py
@@ -10,60 +10,41 @@ os.system("cls")
 print(""" 
       
 --------valor das Frutas---------      
-Fruta	    Até 5 Kg	     Acima de 5 Kg
-Morango	   R$ 2,50 por Kg	R$ 2,20 por Kg
-Maçã	   R$ 1,80 por Kg	R$ 1,50 por Kg
+Fruta	            Até 5 Kg	     Acima de 5 Kg
+Morango	   R$ 2,50 por Kg	    R$  2,20 por Kg
+Maçã	         R$ 1,80 por Kg     R$ 1,50 por Kg
 """) 
 
 print("")
 
-fruta = str(input("""
-                  
-                  
----------Digite a fruta que você deseja-------------
-                  
-
-
-Morango
-                  
-
-""")).lower()
-
-kilos = float(input("Digite a quantidade de KG de morangos que deseja: "))
-
+fruta = str(input("Digite a fruta que você deseja (Morango): ")).lower()
+kilos_morango = float(input("Digite a quantidade de KG de morangos que deseja: "))
 
 match fruta:
     case "morango":
-        if kilos <= 5:
-            valor_morango = kilos * 2.50
-        elif kilos > 5:
-            valor_morango = kilos * 2.20
+        if kilos_morango <= 5:
+            valor_morango = kilos_morango * 2.50
+        else:
+            valor_morango = kilos_morango * 2.20
+    case _:
+        valor_morango = 0
 
-fruta = str(input("""
-                  
-                  
-
----------Digite a fruta que você deseja-------------
-                  
-
-Maca (Maçã)
-                  
-
-""")).lower()
-
-kilos = float(input("Digite a quantidade de KG de maçãs que deseja: "))
-
+fruta = str(input("Digite a fruta que você deseja (Maca): ")).lower()
+kilos_maca = float(input("Digite a quantidade de KG de maçãs que deseja: "))
 
 match fruta:
     case "maca":
-        if kilos <= 5:
-            valor_maca = kilos * 1.80
-        elif kilos > 5:
-            valor_maca = kilos * 1.50
+        if kilos_maca <= 5:
+            valor_maca = kilos_maca * 1.80
+        else:
+            valor_maca = kilos_maca * 1.50
+    case _:
+        valor_maca = 0
 
 valor_total = valor_morango + valor_maca
+peso_total = kilos_morango + kilos_maca
 
-if kilos >= 10 or valor_total > 15:
+if peso_total >= 10 or valor_total > 15:
     valor_total *= 0.90
 
 print(f"Valor total a ser pago: R$ {valor_total:.2f}")
